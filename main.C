b26440845c9f0fe5b74a88acdb7403a7f491d699
Jonatan Ayel <Jonathanayelee@gmail.com>
2026-04-09 17:44:24 +0200

    C kod

---
+++ main.C
@@ -0,0 +1,16 @@
+int main () {
+   
+    int choice; 
+
+    printf("1. Lägg till uppgift\n"); 
+    printf("2. Visa uppgifter\n"); 
+    printf("3. Ta bort uppgift\n"); 
+    printf("4. Avsluta\n"); 
+
+    scanf("%d", &choice); 
+    //hejsan
+    printf("Du valde: %d\n", choice); 
+
+    return 0; 
+
+}
